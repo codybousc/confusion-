70808ec6b717642902035de2e2c69cb70dd643f3
Cody Bouscaren & David Abel <student@epicodus.com>
2015-04-29 11:27:45 -0700

    Finishes unit testing, moving on to intergration

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,34 +1,55 @@
+require('pry')
 class Vehicle
 @@vehicles = []
 
-define_method(:initialize) do |make, model, year|
-    @make = make
-    @model = model
-    @year = year
-end
+  define_method(:initialize) do |make, model, year|
+      @make = make
+      @model = model
+      @year = year
+      @id = @@vehicles.length.+(1)
+  end
 
-define_method(:make) do
-  @make
-end
+  define_method(:make) do
+    @make
+  end
 
-define_method(:model) do
-  @model
-end
+  define_method(:model) do
+    @model
+  end
 
-define_method(:year) do
-  @year
-end
+  define_method(:id) do
+    @id
+  end
 
-define_singleton_method(:all) do
-  @@vehicles
-end
+  define_method(:year) do
+    @year
+  end
 
-define_singleton_method(:save) do
-  @@vehicles.push(self)
-end
+  define_singleton_method(:all) do
+    @@vehicles
+  end
 
-define_singleton_method(:clear) do
-  @@vehichles = []
-end
+  define_method(:save) do
+    @@vehicles.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@vehicles = []
+  end
+
+  define_method(:age) do
+    current_time = Time.new.year
+    @age = current_time - @year
+  end
+
+  define_method(:worth_buying?) do
+    american_made = ["Chevy", "Ford", "GM"]
+      if american_made.include?(@make) && @age < 15
+        true
+      else
+        false
+      end
+  end
+
 
 end
