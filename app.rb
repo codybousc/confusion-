1f206849721f461c002f9d21f5e7f0ae3a653d12
Cody Bouscaren & David Abel <student@epicodus.com>
2015-04-29 16:01:54 -0700

    End of day push. Some Web pages functional.

--- app.rb
+++ app.rb
@@ -8,7 +8,13 @@ get('/') do
 end
 
 get('/vehicles') do
+  @vehicles = Vehicle.all()
   erb(:vehicles)
+end
+
+get('/vehicles/:id') do
+  @vehicle = params.fetch("id").to_i()
+  erb(:vehicle)
 end
 
 get('/add_vehicle') do
@@ -16,17 +22,16 @@ get('/add_vehicle') do
 end
 
 get('/success') do
-  @all_vehicles = @@vehicles.all
+  @vehicle
   erb(:success)
 end
 
-
 post('/success') do
-make = params.fetch("make")
-model = params.fetch("model")
-year = params.fetch("year")
+  make = params.fetch("make")
+  model = params.fetch("model")
+  year = params.fetch("year")
 
-vehicle = Vehicle.new(make, model, year)
-vehicle.save
+  @vehicle = Vehicle.new(make, model, year)
+  @vehicle.save
   erb(:success)
 end
